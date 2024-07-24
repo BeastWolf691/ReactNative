55ee4053cbfad9e39d017fe0885285663206021a
BeastWolf691 <sriver@hotmail.fr>
2024-07-24 13:36:30 +0200

    update

--- app/data-dog.js
+++ app/data-dog.js
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList, Pressable, Button } from "react-native";
 import axios from "axios";
 
@@ -7,6 +7,7 @@ export default function DataDog() {
     const [dogPosition, setDogPosition] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [page, setPage] = useState(1);
+    const list = useRef();
 
     useEffect(() => {
         // Fonction asynchrone pour obtenir les données
@@ -16,9 +17,9 @@ export default function DataDog() {
                     headers: {
                         'ngrok-skip-browser-warning': 'true' // nécessaire seulement car hébergeur du serveur
                     },
-                    params: {page}
+                    params: { page }
                 });
-                setDogPosition([...dogPosition,...response.data]);//permet de rajouter a la suite les autres chiens en appuyant sur le bouton
+                setDogPosition([...dogPosition, ...response.data]);//permet de rajouter a la suite les autres chiens en appuyant sur le bouton
                 setIsLoading(false);
             } catch (error) {
                 console.error("Erreur lors de la récupération des données : ", error);
@@ -36,7 +37,7 @@ export default function DataDog() {
         </View>
     );
 
-    function refreshHandle(){
+    function refreshHandle() {
         setPage(1);
         setDogPosition([]);
         fetchData();
@@ -45,16 +46,17 @@ export default function DataDog() {
         <View style={styles.container}>
             <Link href="/" style={styles.link}>Home</Link>
             {/* <Button title='suivant'style={styles.button} onPress={() => setPage(page + 1)}/> permet lors de l'appuie que les chiens s'ajoutent */}
-            <Pressable>
+            <Pressable onPress={() => list.current.scrollToIndex({index:0})} style={styles.floatingButton}>{/*besoin de renseigner l'objet avec un argument index*/}
                 <Text>↑</Text>
             </Pressable>
-            
+
             {dogPosition.length > 0 ? (
                 <FlatList
+                    ref={list}
                     data={dogPosition}
                     refreshing={isLoading}
                     onRefresh={refreshHandle}
-                    onEndReached={() => setPage(page+1)}
+                    onEndReached={() => setPage(page + 1)}
                     onEndReachedThreshold={3}
                     keyExtractor={item => item.id.toString()}
                     renderItem={renderItem}
@@ -62,7 +64,7 @@ export default function DataDog() {
             ) : (
                 <Text style={styles.loadingText}>Chargement des données...</Text>
             )}
-           
+
         </View>
     );
 }
@@ -105,4 +107,16 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: 'white',
     },
+    floatingButton: {
+        padding: 20,
+        borderRadius: 100,
+        backgroundColor: 'skyblue',
+        fontSize: 20,
+        fontWeight: 'bold',
+        position: 'absolute',
+        bottom: 5,
+        right: 10,
+        width: 25,
+        zIndex: 1
+    }
 });
